8b3ce2cfd8f4c11da947fc1f3c5948ef60afcedf
esp0x309 <sebastian.nawrot@outlook.com>
2021-01-21 00:18:29 +0100

    Add some code to wrap text program

--- wrap_lines.c
+++ wrap_lines.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
-#define MAXLINELENGTH 80
+#define WRAPLINE 4 
 #define MAXLINE 1000
-#define MAXLINES 1000
+#define MAXLENGTH 10000 
 
 /* program to wrap lines */
 int getline(char line[], int limit);
+int length(char str[]);
+void mergeLines(char from[], char to[]);
 void wrapLines(char str[], int maxLineLength);
 
 int main()
 {
     char line[MAXLINE]; 
-    char wrappedLines[MAXLINES];
+    char mergedLines[MAXLENGTH];
 
-    while ((getline(line, MAXLINE)) > 0)
-        wrapLines(line, MAXLINELENGTH);
+    while ((getline(line, MAXLINE)) > 0) { /* for every input line */
+        mergeLines(line, mergedLines); /* merge them with others */
+    }
+
+    /* printf("%s", mergedLines); */
+    wrapLines(mergedLines, WRAPLINE); 
 
     return 0;
 }
@@ -34,8 +40,42 @@ int getline(char s[], int limit)
     return i;
 }
 
+int length(char str[])
+{
+    int i;
+    i = 0;
+
+    while (str[++i] != '\0') {} 
+
+    return i-1; /* -1 because last char is \n and we don't want count him */
+}
+
+void mergeLines(char from[], char to[])
+{
+    int i;
+    int shift;
+
+    i = 0;
+    shift = 0;
+
+    while (to[i++] != '\0') { /* check where where is end of string to  */
+        ++shift; /* merge next line to end of prev */ 
+    }
+
+    for (i = 0; i < length(from); i++) { /* merging */
+        to[i + shift] = from[i];
+    }
+    /* if line doesnt ended by ' ' or \t then add ' ' to separate lines */
+    if (to[i - 1 + shift] != ' ' && to[i - 1 + shift] != '\t') /* last char of to */
+        to[i + shift] = ' ';
+}
 void wrapLines(char str[], int maxLineLength)
 {
     /* TO DO */
-    printf("%s", wrappedLines);
+    int i;
+    for (i = 0; i <= length(str); i++) {
+        printf("%c", str[i]);
+        if ((i+1) % maxLineLength == 0)
+            printf("\n");
+    }
 }
